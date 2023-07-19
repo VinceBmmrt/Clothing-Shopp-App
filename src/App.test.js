241332ab7e2c3b9014491e5c08f027576ba87c14
Vince Bmmrt <vincebommert75@gmail.com>
2023-07-19 16:50:24 +0200

    re add test jest

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,8 @@ import {
 } from "@testing-library/react";
 import App from "./App";
 
-// test("renders learn react link", () => {
-//   root.render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders learn react link", () => {
+  root.render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
