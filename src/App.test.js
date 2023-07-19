450502d506b1c2abd922064e82af2632615b13c1
Vince Bmmrt <vincebommert75@gmail.com>
2023-07-19 16:36:23 +0200

    removing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import { createRoot } from "react-dom/client";
-import { screen } from "@testing-library/react";
+import {
+  createRoot
+} from "react-dom/client";
+import {
+  screen
+} from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  root.render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test("renders learn react link", () => {
+//   root.render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
