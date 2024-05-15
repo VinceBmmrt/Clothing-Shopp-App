710820afb96e535ed40d0496d88927a894492e6f
VinceBmmrt <vincebommert75@gmail.com>
2024-05-15 13:07:51 +0200

    fix pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,9 @@
-import {
-  createRoot
-} from "react-dom/client";
-import {
-  screen
-} from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 test("renders learn react link", () => {
+  const root = createRoot(document.createElement("div"));
   root.render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
