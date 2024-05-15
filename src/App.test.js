b122c18e59be5871522ca6a2678376a78d082216
VinceBmmrt <vincebommert75@gmail.com>
2024-05-15 13:12:17 +0200

    fixbug2

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
-import { screen } from "@testing-library/react";
-import { createRoot } from "react-dom/client";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const root = createRoot(document.createElement("div"));
-  root.render(<App />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
